149d9cad426d9b9c15138d1ad87bec8322daced2
alessrdrgz <alessrodrgz@gmail.com>
2022-07-25 18:00:36 +0200

    :sparkles:feat: paused screen component for sudoku

---
+++ src/lib/components/sudoku/sudoku-paused-screen.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import PlayIcon from '@icons/play.svelte';
+	import { sudoku } from '@store/sudoku';
+
+	const handleClick = () => {
+		$sudoku.paused = !$sudoku.paused;
+	};
+</script>
+
+<div class="flex absolute h-full w-full items-center justify-center" on:click={handleClick}>
+	<div class="rounded-full bg-blue-400 w-16 h-16">
+		<PlayIcon class="fill-white" />
+	</div>
+</div>
