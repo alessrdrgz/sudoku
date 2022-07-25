09917ac5644d932b90645b25354821e893d64755
alessrdrgz <alessrodrgz@gmail.com>
2022-07-25 18:03:19 +0200

    :sparkles:feat: difficulty and timer on header

--- src/lib/components/sudoku/header/sudoku-header.svelte
+++ src/lib/components/sudoku/header/sudoku-header.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import AutoCheckToggle from '@sudoku/autocheck-toggle.svelte';
+	import { sudoku } from '@store/sudoku';
+	import Timer from '@sudoku/header/timer.svelte';
 </script>
 
-<div class="flex w-[400px] items-center">
-	<AutoCheckToggle />
-</div>
+<section class="flex w-[400px] items-center text-gray-300 p-2">
+	<div class="text-left">{$sudoku.difficulty}</div>
+	<!-- TODO Error counting -->
+	<div class="flex-grow justify-center text-center">Errores 0/3</div>
+	<div class="justify-end"><Timer /></div>
+</section>
