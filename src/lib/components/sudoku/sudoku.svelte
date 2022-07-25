b9fb949b15b0e3f863e690401644774119fb444b
alessrdrgz <alessrodrgz@gmail.com>
2022-07-25 18:03:02 +0200

    :sparkles:feat: show paused screen when game is paused

--- src/lib/components/sudoku/sudoku.svelte
+++ src/lib/components/sudoku/sudoku.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { sudoku } from '@store/sudoku';
 	import SudokuBox from '@sudoku/sudoku-box.svelte';
+	import SudokuPausedScreen from '@sudoku/sudoku-paused-screen.svelte';
 	export let puzzle: Array<Array<string>>;
 </script>
 
-<div class="flex flex-wrap w-[400px] h-[400px] border-2 border-black">
+<div class="flex flex-wrap w-[400px] h-[400px] border-2 border-black relative">
 	{#each puzzle as box, boxIndex}
 		<div
 			class="flex flex-wrap flex-grow flex-shrink-0 bg-white border border-black basis-1/3"
@@ -12,4 +14,8 @@
 			<SudokuBox {box} {boxIndex} />
 		</div>
 	{/each}
+
+	{#if $sudoku.paused}
+		<SudokuPausedScreen />
+	{/if}
 </div>
