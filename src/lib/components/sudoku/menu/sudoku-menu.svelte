a181541ab8c23454b2602d4c74915a6a740defbc
alessrdrgz <alessrodrgz@gmail.com>
2022-07-26 02:56:04 +0200

    :lipstick:style: moved buttons to center and added autocheck toggle

--- src/lib/components/sudoku/menu/sudoku-menu.svelte
+++ src/lib/components/sudoku/menu/sudoku-menu.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import HintButton from '@sudoku/menu/buttons/hint-button.svelte';
 	import ResetButton from '@sudoku/menu/buttons/reset-button.svelte';
+	import AutocheckToggle from '../autocheck-toggle.svelte';
 </script>
 
-<div class="flex w-[400px] p-4 gap-4">
-	<ResetButton />
-	<HintButton />
+<div class="flex w-[400px] p-4 gap-2 flex-col">
+	<div class="flex-grow">
+		<AutocheckToggle />
+	</div>
+	<div class="flex p-4 gap-4 flex-grow items-center justify-center">
+		<ResetButton />
+		<HintButton />
+	</div>
 </div>
