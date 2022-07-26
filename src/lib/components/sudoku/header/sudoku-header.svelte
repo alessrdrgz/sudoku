9a74c5fc217a5cdff8094aa84a26d8c93e435cc4
alessrdrgz <alessrodrgz@gmail.com>
2022-07-26 02:55:35 +0200

    :sparkles:feat: show error number from sudoku store

--- src/lib/components/sudoku/header/sudoku-header.svelte
+++ src/lib/components/sudoku/header/sudoku-header.svelte
@@ -5,7 +5,6 @@
 
 <section class="flex w-[400px] items-center text-gray-300 p-2">
 	<div class="text-left">{$sudoku.difficulty}</div>
-	<!-- TODO Error counting -->
-	<div class="flex-grow justify-center text-center">Errores 0/3</div>
+	<div class="flex-grow justify-center text-center">Errores {$sudoku.errors}/3</div>
 	<div class="justify-end"><Timer /></div>
 </section>
