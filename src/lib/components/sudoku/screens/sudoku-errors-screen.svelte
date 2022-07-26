ff68beef124de55ddae33a8937b613431575b78e
alessrdrgz <alessrodrgz@gmail.com>
2022-07-26 02:56:23 +0200

    :sparkles:feat: sudoku errors screen

---
+++ src/lib/components/sudoku/screens/sudoku-errors-screen.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import SudokuScreen from '@components/sudoku/screens/sudoku-screen.svelte';
+</script>
+
+<SudokuScreen>
+	<div class="flex items-center justify-center p-4">
+		<h2 class="text-2xl font-semibold text-center">Ha superado el número máximo de errores</h2>
+	</div>
+</SudokuScreen>
