e55ae283748dee1e2849d75a252e5621bee55b97
alessrdrgz <alessrodrgz@gmail.com>
2022-07-26 02:18:36 +0200

    :sparkles:feat: paused game screen

---
+++ src/lib/components/sudoku/screens/sudoku-paused-screen.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import SudokuScreen from '@components/sudoku/screens/sudoku-screen.svelte';
+	import PlayIcon from '@icons/play.svelte';
+	import { sudoku } from '@store/sudoku';
+
+	const handleClick = () => {
+		$sudoku.paused = !$sudoku.paused;
+	};
+</script>
+
+<SudokuScreen>
+	<div class="rounded-full bg-blue-400 w-16 h-16 cursor-pointer" on:click={handleClick}>
+		<PlayIcon class="fill-white" />
+	</div>
+</SudokuScreen>
