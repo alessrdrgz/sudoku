d06cddaaee4275fcc81e33b30c546b894cd22012
alessrdrgz <alessrodrgz@gmail.com>
2022-07-26 02:17:43 +0200

    :sparkles:feat: sudoku finished screen

---
+++ src/lib/components/sudoku/screens/sudoku-finished-screen.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import SudokuScreen from '@sudoku/screens/sudoku-screen.svelte';
+</script>
+
+<SudokuScreen>
+	<div>
+		<h1 class="text-xl font-semibold text-center">Felicidades, has completado el Sudoku</h1>
+	</div>
+</SudokuScreen>
