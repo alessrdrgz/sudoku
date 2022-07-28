1b9b0ed0c29babe5da778d01884b07d1ad2dd712
alessrdrgz <alessrodrgz@gmail.com>
2022-07-29 01:24:10 +0200

    :sparkles:feat: show disabled sudoku on reaching the maximum number of errors

--- src/lib/components/sudoku/sudoku-box.svelte
+++ src/lib/components/sudoku/sudoku-box.svelte
@@ -8,7 +8,9 @@
 
 {#each box as n, index}
 	<div class="flex-grow flex-shrink-0 basis-1/3">
-		{#if $sudoku.paused || $sudoku.finished}
+		{#if $sudoku.errors === 3}
+			<SudokuCell value={n} box={boxIndex} {index} disabled={true} />
+		{:else if ($sudoku.paused || $sudoku.finished) && $sudoku.errors !== 3}
 			<SudokuCell value={''} box={boxIndex} {index} />
 		{:else}
 			<SudokuCell value={n} box={boxIndex} {index} />
