5f11c76ba5e4b80cde46b814cbc856232d36b431
alessrdrgz <alessrodrgz@gmail.com>
2022-07-29 01:22:36 +0200

    :lipstick:style: adding a header stating that the maximum number of errors has reached

--- src/lib/components/sudoku/header/sudoku-header.svelte
+++ src/lib/components/sudoku/header/sudoku-header.svelte
@@ -3,6 +3,10 @@
 	import Timer from '@sudoku/header/timer.svelte';
 </script>
 
+<h1 class={`text-center text-red-500 text-lg font-semibold ${$sudoku.errors !== 3 && 'invisible'}`}>
+	Ha superado el número máximo de errores.
+</h1>
+
 <section class="flex w-[400px] items-center text-gray-300 p-2">
 	<div class="text-left">{$sudoku.difficulty}</div>
 	<div class={`flex-grow justify-center text-center ${$sudoku.errors === 3 && 'text-red-500'}`}>
