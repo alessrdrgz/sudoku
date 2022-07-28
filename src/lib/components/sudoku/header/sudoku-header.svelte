7a699dfb301327739d1e830f8971a4fbc03c7b55
alessrdrgz <alessrodrgz@gmail.com>
2022-07-29 01:22:09 +0200

    :lipstick:style: set error text to red when reaching the max number

--- src/lib/components/sudoku/header/sudoku-header.svelte
+++ src/lib/components/sudoku/header/sudoku-header.svelte
@@ -5,6 +5,8 @@
 
 <section class="flex w-[400px] items-center text-gray-300 p-2">
 	<div class="text-left">{$sudoku.difficulty}</div>
-	<div class="flex-grow justify-center text-center">Errores {$sudoku.errors}/3</div>
+	<div class={`flex-grow justify-center text-center ${$sudoku.errors === 3 && 'text-red-500'}`}>
+		Errores {$sudoku.errors}/3
+	</div>
 	<div class="justify-end"><Timer /></div>
 </section>
